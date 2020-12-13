905b2e7469061d5b995d6e9e247e44a6e22aeece
Yadukrishnan Nair <33169773+ydnair007@users.noreply.github.com>
2020-12-13 06:01:04 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,6 +1,6 @@
 class Creds():
     # ENTER Your bot Token Here
-    TG_TOKEN = "1413272769:AAHg4aE6To1wVzDOLrGvyjdV7lJC77RPaSY"
+    TG_TOKEN = "1436489993:AAGXKRFV5o5dEJtZHVbSpiNgHvJ7AQ2FVIg"
     
     
     
